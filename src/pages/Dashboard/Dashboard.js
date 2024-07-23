160b562e393b94f2c5e8a9e6f10788541e7a843c
amaliaarifahh <amaliaarifahh02@mail.ugm.ac.id>
2024-07-23 12:03:40 +0700

    Update Dashboard for responsive design

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavigationBar from "../../components/NavigationBar";
 import "./Dashboard.css";
 
 const Dashboard = () => {
+  const [iframeSrc, setIframeSrc] = useState("");
+
+  const updateIframeSrc = () => {
+    if (window.matchMedia("(max-width: 480px)").matches) {
+      setIframeSrc(
+        "https://lookerstudio.google.com/embed/reporting/a11b3e6b-e4d1-4555-910c-6bd403b83ac1/page/uXtyD"
+      );
+    } else if (window.matchMedia("(max-width: 768px)").matches) {
+      setIframeSrc(
+        "https://lookerstudio.google.com/embed/reporting/a11b3e6b-e4d1-4555-910c-6bd403b83ac1/page/uXtyD"
+      );
+    } else {
+      setIframeSrc(
+        "https://lookerstudio.google.com/embed/reporting/725aa366-ae25-4ecb-8831-d80e4e2b8ad0/page/uXtyD"
+      );
+    }
+  };
+
+  useEffect(() => {
+    updateIframeSrc();
+    window.addEventListener("resize", updateIframeSrc);
+
+    return () => {
+      window.removeEventListener("resize", updateIframeSrc);
+    };
+  }, []);
+
   return (
     <div>
       <NavigationBar />
       <section className="dashboard-page" id="dashboard-page">
         <div className="dashboard-container">
-          {/* iframe dari Google Looker Studio*/}
           <iframe
             title="Dashboard Kependudukan"
-            width="100%"
-            height="100%"
-            src="https://lookerstudio.google.com/embed/reporting/725aa366-ae25-4ecb-8831-d80e4e2b8ad0/page/uXtyD"
+            className="dashboard-iframe"
+            src={iframeSrc}
             frameborder="0"
-            style={{
-              border: "0",
-              borderRadius: "30px",
-              boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.1)",
-            }}
             allowfullscreen
             sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
           ></iframe>

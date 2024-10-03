04adcc38bc1142bf2a5d48931476a6cf986b4c70
amaliaarifahh <amaliaarifahh02@mail.ugm.ac.id>
2024-10-03 22:23:45 +0700

    Menambahkan referensi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,16 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>© 2024 Tegal Spatial. All rights reserved.</p>
-      <p>Created by Amalia Arifah Puji Sopyan</p>
+      <p>
+        Created by Amalia Arifah Puji Sopyan. Reference to{" "}
+        <a
+          href="https://github.com/yakubhariana70/spatialcitizen"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Yakub Hariana
+        </a>
+      </p>
     </footer>
   );
 };
